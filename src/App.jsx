68d4615b4728b4202cdd51c0af8d1caa527555bd
Ashish Sharma <new_email@example.com>
2025-01-15 20:05:21 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { HashRouter, Routes, Route } from "react-router-dom"
 
-function App() {
-  const [count, setCount] = useState(0)
+import Home from './Home.jsx'
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+import Login from './screens/auth/login/login.jsx'
+import Signup from './screens/auth/signup/signup.jsx'
+import Dashboard from './screens/dashboard/dashboard.jsx'
+
+import NoPage from './NoPage.jsx'
+
+let App = () => <>
+  <HashRouter>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/*" element={<NoPage />} />
+      <Route path="/" element={<Home />} />
+      </Routes>
+  </HashRouter>
+</>
 
 export default App
